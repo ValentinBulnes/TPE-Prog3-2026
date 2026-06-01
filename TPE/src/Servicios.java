0c8bd5687070d0603d2ea80f5981445de26101b5
ValentinBulnes <bulnesvalentin@gmail.com>
2026-06-01 16:29:58 -0300

    complejidad y fix servicio3

--- TPE/src/Servicios.java
+++ TPE/src/Servicios.java
@@ -3,12 +3,11 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Servicios {
-    //Completar con las estructuras y métodos privados que se requieran.
 
     private List<Camion> camiones;
     private HashMap<String, Paquete> paquetesPorCodigo;
 
-    //Expresar la complejidad temporal del constructor.
+    //Complejidad Temporal Constructor: O(c + p) c cantidad de camiones, p cantidad de paquetes
     public Servicios(String pathCamiones, String pathPaquetes) {
         camiones = CSVParser.parsearCamiones(pathCamiones);
         paquetesPorCodigo = new HashMap<>();
@@ -19,16 +18,14 @@ public class Servicios {
         }
     }
 
-   //Expresar la complejidad temporal del servicio 1. 
-   //               O(1)??
+   //Complejidad Temporal Servicio1: O(1)
     public Paquete servicio1(String codigoPaquete) {
         Paquete salida = (paquetesPorCodigo.containsKey(codigoPaquete)) ? paquetesPorCodigo.get(codigoPaquete) : null;
         return salida;
     }
 
 
-    //Expresar la complejidad temporal del servicio 2.
-    //               O(n)??
+    //Complejidad Temporal Servicio2: O(n) n cantidad de paquetes
     public List<Paquete> servicio2(boolean contieneAlimentos) {
         List<Paquete> salida = new ArrayList<>();
         for(String codigo : paquetesPorCodigo.keySet()) {
@@ -38,13 +35,12 @@ public class Servicios {
         return salida;
     }
 
-    //Expresar la complejidad temporal del servicio 3.
-    //                O(n)??
+    //Complejidad Temporal Servicio3: O(n) n cantidad de paquetes
     public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {
         List<Paquete> salida = new ArrayList<>();
         for(String codigo : paquetesPorCodigo.keySet()) {
             Paquete p = paquetesPorCodigo.get(codigo);
-            if(p.getNivelUrgencia() >= urgenciaMaxima && p.getNivelUrgencia() <= urgenciaMaxima) salida.add(p);
+            if(p.getNivelUrgencia() >= urgenciaMinima && p.getNivelUrgencia() <= urgenciaMaxima) salida.add(p);
         }
         return salida;
     }
